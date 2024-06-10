72503648139b63fa2c1bae365e50b554a6d563cb
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-10 10:23:06 +0200

    Ajout de findAll dans TVshowCollection

--- src/Entity/Collection/TVshowCollection.php
+++ src/Entity/Collection/TVshowCollection.php
@@ -1,10 +1,28 @@
 <?php
 declare(strict_types=1);
+
 namespace Entity\Collection;
 use Database\MyPdo;
 use Entity\TVshow;
+
 use PDO;
 class TVshowCollection
 {
-
+    /**
+     * Méthode qui retourne un tableau contenant tous les tvshows triés par ordre alphabétique
+     *
+     * @return TVshow[]|false
+     */
+    public static function findAll():array|false
+    {
+        $req = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+    SELECT *
+    FROM tvshow
+    ORDER BY name
+SQL
+        );
+        $req->execute();
+        return $req->fetchAll(PDO::FETCH_CLASS, TVshow::class);
+    }
 }
