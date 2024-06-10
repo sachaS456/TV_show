7b0c02b6e7fde23ce7636245824167a9ec40688c
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-10 16:14:21 +0200

    Ajout des tests sur les paramètres de season.php

--- public/season.php
+++ public/season.php
@@ -3,3 +3,21 @@ declare(strict_types=1);
 
 use Html\AppWebPage;
 use Entity\Collection\TVshowCollection;
+use Database\MyPdo;
+use Entity\Season;
+use Entity\TVshow;
+use Entity\Poster;
+
+if (!isset($_GET['TvshowId']) || !is_numeric($_GET['TvshowId'])) {
+    header('Location: ./index.php');
+    exit();
+} else {
+    $tvShowId = (int) $_GET['TvshowId'];
+}
+
+try {
+    $stmt = TVshow::findById($tvShowId);
+} catch (\Entity\Exception\EntityNotFoundException) {
+    http_response_code(404);
+    exit();
+}
