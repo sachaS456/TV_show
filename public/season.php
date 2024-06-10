7cd59500dc145f71d421cf4ac1e0e78cd11a7e64
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-10 16:52:39 +0200

    modification des noms des classes

--- public/season.php
+++ public/season.php
@@ -28,12 +28,12 @@ $webPage = new AppWebpage("Séries TV :  {$stmt->getName()}");
 $webPage->appendContent("<div  class=\"list\">");
 
 $posterTvShowId = Poster::findById($stmt->getPosterId())->getId();
-$webPage->appendContent("<div class=\"season\"><div class=\"tvshow__poster\"><img src='./poster.php?posterId=$posterTvShowId'></div><div class='tvshow__title'>{$stmt->getName()}</div><div class='tvshow__original'>{$stmt->getOriginalName()}</div><div class='tvshow__description'>{$stmt->getOverview()}</div></div>");
+$webPage->appendContent("<div class=\"season\"><div class=\"season__poster\"><img src='./poster.php?posterId=$posterTvShowId'></div><div class='season__title'>{$stmt->getName()}</div><div class='season__original'>{$stmt->getOriginalName()}</div><div class='season__description'>{$stmt->getOverview()}</div></div>");
 
 foreach ($stmt->getSeasons() as $season) {
     $seas = $webPage->escapeString($season->getName());
     $posterId = Poster::findById($season->getPosterId())->getId();
-    $webPage->appendContent("<div class =\"tvshow\"><div class =\"tvshow__poster\"><img src='./poster.php?posterId=$posterId'></div><div class = \"tvshow__title\">$seas</div></div>");
+    $webPage->appendContent("<div class =\"season\"><div class =\"season__poster\"><img src='./poster.php?posterId=$posterId'></div><div class = \"season__title\">$seas</div></div>");
 }
 
 $webPage->appendContent("</div>");
