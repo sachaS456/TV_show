a56adf0c740b5fa288f44c4beb93240a7a0e32f1
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-10 09:35:56 +0200

    ajout de documentation

--- src/Entity/TVshow.php
+++ src/Entity/TVshow.php
@@ -16,65 +16,123 @@ class TVshow
     private string $overview;
     private int $posterId;
 
+    /**
+     * Id getter
+     *
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * Id Setter
+     * @param int|null $id
+     * @return void*
+     */
     public function setId(?int $id = null): void
     {
         $this->id = $id;
     }
 
+    /**
+     * Name getter
+     *
+     * @return string
+     */
     public function getName(): string
     {
         return $this->name;
     }
 
+    /**
+     * Name setter
+     * @param string $name
+     * @return void
+     */
     public function setName(string $name): void
     {
         $this->name = $name;
     }
 
+    /**
+     * OriginalName getter
+     * @return string
+     */
     public function getOriginalName(): string
     {
         return $this->originalName;
     }
 
+    /**
+     * originalName setter
+     * @param string $originalName
+     * @return void
+     */
     public function setOriginalName(string $originalName): void
     {
         $this->originalName = $originalName;
     }
 
+    /**
+     * Homepage getter
+     * @return string
+     */
     public function getHomepage(): string
     {
         return $this->homepage;
     }
 
+    /**
+     * homepage setter
+     * @param string $homepage
+     * @return void
+     */
     public function setHomepage(string $homepage): void
     {
         $this->homepage = $homepage;
     }
 
+    /**
+     * Overview getter
+     * @return string
+     */
     public function getOverview(): string
     {
         return $this->overview;
     }
 
+    /**
+     * Overview setter
+     * @param string $overview
+     * @return void
+     */
     public function setOverview(string $overview): void
     {
         $this->overview = $overview;
     }
 
+    /**
+     * posterId getter
+     * @return int
+     */
     public function getPosterId(): int
     {
         return $this->posterId;
     }
 
+    /**
+     * poster id setter
+     * @param int $posterId
+     * @return void
+     */
     public function setPosterId(int $posterId): void
     {
         $this->posterId = $posterId;
     }
+
+
 
 
 
