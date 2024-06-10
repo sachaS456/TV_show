c951a168e9b2e1cd513648a0a042a4d85405c7b8
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-10 10:31:56 +0200

    Ajout des setters et getters dans Season.php

--- src/Entity/Season.php
+++ src/Entity/Season.php
@@ -4,5 +4,60 @@ namespace Entity;
 
 class Season
 {
+    private int $id;
+    private int $tvShowId;
+    private string $name;
+    private int $seasonNumber;
+    private int $posterId;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getTvShowId(): int
+    {
+        return $this->tvShowId;
+    }
+
+    public function setTvShowId(int $tvShowId): void
+    {
+        $this->tvShowId = $tvShowId;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getSeasonNumber(): int
+    {
+        return $this->seasonNumber;
+    }
+
+    public function setSeasonNumber(int $seasonNumber): void
+    {
+        $this->seasonNumber = $seasonNumber;
+    }
+
+    public function getPosterId(): int
+    {
+        return $this->posterId;
+    }
+
+    public function setPosterId(int $posterId): void
+    {
+        $this->posterId = $posterId;
+    }
 
 }
