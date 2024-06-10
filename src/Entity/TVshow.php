e3f1a2a4af4033c94ccf27afcb9ccf1e9b90057f
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-10 16:35:30 +0200

    findById mis en static

--- src/Entity/TVshow.php
+++ src/Entity/TVshow.php
@@ -139,7 +139,7 @@ class TVshow
      * @param int $id
      * @return TVshow
      */
-    public function findById(int $id): TVshow // EntityNotFoundException
+    public static function findById(int $id): TVshow // EntityNotFoundException
     {
         $req = MyPdo::getInstance()->prepare(
             <<<'SQL'
