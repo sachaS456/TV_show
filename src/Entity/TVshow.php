080b49e1c19752b7059b70bc4d97a9d3eda78e98
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-10 09:29:43 +0200

    ajout de la structure de base (attributs + getters & setters) dans TVshow

--- src/Entity/TVshow.php
+++ src/Entity/TVshow.php
@@ -7,8 +7,75 @@ use Entity\Collection\AlbumCollection;
 use Entity\Exception\EntityNotFoundException;
 use PDO;
 
-
 class TVshow
 {
+    private ?int $id;
+    private string $name;
+    private string $originalName;
+    private string $homepage;
+    private string $overview;
+    private int $posterId;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id = null): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getOriginalName(): string
+    {
+        return $this->originalName;
+    }
+
+    public function setOriginalName(string $originalName): void
+    {
+        $this->originalName = $originalName;
+    }
+
+    public function getHomepage(): string
+    {
+        return $this->homepage;
+    }
+
+    public function setHomepage(string $homepage): void
+    {
+        $this->homepage = $homepage;
+    }
+
+    public function getOverview(): string
+    {
+        return $this->overview;
+    }
+
+    public function setOverview(string $overview): void
+    {
+        $this->overview = $overview;
+    }
+
+    public function getPosterId(): int
+    {
+        return $this->posterId;
+    }
+
+    public function setPosterId(int $posterId): void
+    {
+        $this->posterId = $posterId;
+    }
+
+
 
 }
