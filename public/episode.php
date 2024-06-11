1589cddb2cf8567ac9ab510ac69df168d490a186
Sacha Himber <sacha.himber@etudiant.univ-reims.fr>
2024-06-11 09:08:50 +0200

    pisode.php: vérification du parametre seasonId

--- public/episode.php
+++ public/episode.php
@@ -9,4 +9,29 @@ use Entity\Season;
 use Entity\TVshow;
 use Entity\Episode;
 use Entity\Poster;
+use Exception\ParameterException;
 
+try{
+    if (!isset($_GET['seasonId']) || empty($_GET['seasonId']) || !is_numeric($_GET['seasonId']))
+    {
+        throw new ParameterException('parameter is not valid');
+    }
+    else{
+        $tvShowId = (int) $_GET['seasonId'];
+
+        // next code
+    }
+}
+catch (ParameterException)
+{
+    header('Location: ./index.php');
+    exit();
+}
+catch (EntityNotFoundException)
+{
+    http_response_code(404);
+}
+catch (Exception)
+{
+    http_response_code(500);
+}
