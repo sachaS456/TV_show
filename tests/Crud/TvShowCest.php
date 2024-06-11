ffffee327cbaa1c597db8b2b12ab7cab954ee429
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-11 16:41:27 +0200

    test Find by id throws exception if artist does not exist OK

--- tests/Crud/TvShowCest.php
+++ tests/Crud/TvShowCest.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace Crud;
+
 use Entity\TVshow;
 use Entity\Exception\EntityNotFoundException;
 use Tests\CrudTester;
+
 class TvShowCest
 {
     public function findById(CrudTester $I)
@@ -12,8 +14,15 @@ class TvShowCest
         $I->assertSame(3, $tvShow->getId());
         $I->assertSame('Friends', $tvShow->getName());
         $I->assertSame('Friends', $tvShow->getOriginalName());
-        $I->assertSame('https://www.netflix.com/title/70153404',$tvShow->getHomepage());
-        $I->assertSame('Les péripéties de 6 jeunes newyorkais liés par une profonde amitié. Entre amour, travail, famille, ils partagent leurs bonheurs et leurs soucis au Central Perk, leur café favori...',$tvShow->getOverview());
+        $I->assertSame('https://www.netflix.com/title/70153404', $tvShow->getHomepage());
+        $I->assertSame('Les péripéties de 6 jeunes newyorkais liés par une profonde amitié. Entre amour, travail, famille, ils partagent leurs bonheurs et leurs soucis au Central Perk, leur café favori...', $tvShow->getOverview());
         $I->assertSame(15, $tvShow->getPosterId());
+    }
+
+    public function findByIdThrowsExceptionIfArtistDoesNotExist(CrudTester $I)
+    {
+        $I->expectThrowable(EntityNotFoundException::class, function () {
+            TVshow::findById(PHP_INT_MAX);
+        });
     }
 }
