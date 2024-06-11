f0cda93386e06c0ec965881f89ee1ecda0d16ff5
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-11 12:05:37 +0200

    FindById de genre réalisé

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -4,6 +4,7 @@ namespace Entity;
 
 use Database\MyPdo;
 use Entity\Exception\EntityNotFoundException;
+
 class Genre
 {
     private int $id;
@@ -29,5 +30,30 @@ class Genre
         $this->name = $name;
     }
 
+    /**
+     * By Id Genre Finder
+     *
+     * @param int $genreId
+     * @return array
+     */
+    public static function findByGenre(int $genreId): array
+    {
+        $req = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+            SELECT T.id, T.name, T.originalName, T.homepage, T.overview, t.posterId
+            FROM tvshow T 
+                JOIN tvshow_genre TG ON TG.id = T.id
+                JOIN GENRE G ON G.id = TG.genreId
+            WHERE G.id = :gid
+            ORDER BY T.name;
+SQL
+        );
+        $req->execute(['gid' => $genreId]);
+        $res = $req->fetchObject(Genre::class);
 
+        if ($res === false) {
+            throw new EntityNotFoundException("findById : $genreId doesn't exist ");
+        }
+        return $res;
+    }
 }
