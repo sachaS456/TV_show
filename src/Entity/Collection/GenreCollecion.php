d45a33150184e1364d8e314d856e4fcc1a517f05
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-11 12:10:56 +0200

    findAll de GenreCollection OK

--- src/Entity/Collection/GenreCollecion.php
+++ src/Entity/Collection/GenreCollecion.php
@@ -7,5 +7,21 @@ use Entity\Season;
 use PDO;
 class GenreCollecion
 {
+    /**
+     * Method you use to get all the Genres.
+     * @return array
+     */
+    public static function findAll():array
+    {
+        $req = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+            SELECT * 
+            FROM genre
+SQL
+        );
+        $req->execute();
+        return $req;
+    }
+
 
 }
