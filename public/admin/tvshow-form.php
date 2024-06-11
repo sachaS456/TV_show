e67e56e9f517807988a52869dc58aa9658d7cb00
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-11 09:42:44 +0200

    Ajout de la structure try catch

--- public/admin/tvshow-form.php
+++ public/admin/tvshow-form.php
@@ -4,3 +4,12 @@ declare(strict_types=1);
 use Entity\TVshow;
 use Entity\Exception\EntityNotFoundException;
 use Exception\ParameterException;
+try{
+
+} catch (ParameterException) {
+http_response_code(400);
+} catch (EntityNotFoundException) {
+http_response_code(404);
+} catch (Exception) {
+http_response_code(500);
+}
