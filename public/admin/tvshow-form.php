b6f251af2a73391c519aa4f8d7981bd20d9643cc
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-11 09:45:53 +0200

    ajout de la structure du formulaire

--- public/admin/tvshow-form.php
+++ public/admin/tvshow-form.php
@@ -4,8 +4,21 @@ declare(strict_types=1);
 use Entity\TVshow;
 use Entity\Exception\EntityNotFoundException;
 use Exception\ParameterException;
-try{
 
+try{
+    if (!isset($_GET['artistId'])) {
+        $artist = null;
+
+
+    } else {
+        if (!is_numeric($_GET['artistId'])) {
+            throw new ParameterException('artist-form.php : valeur de l\'artiste incorrecte');
+        }
+        $artist = Artist::findById((int)$_GET['artistId']);
+    }
+    $form = new \Html\Form\ArtistForm($artist);
+    $html = new \Html\AppWebPage('Form', '', $form->getHtmlForm($url = 'artist-save.php'));
+    echo $html->toHTML();
 } catch (ParameterException) {
 http_response_code(400);
 } catch (EntityNotFoundException) {
