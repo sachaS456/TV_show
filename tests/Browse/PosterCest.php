25ee391e6a852ff8c38757f96dc2dd0cf23f11cf
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-12 10:06:34 +0200

    load cover with wrong parameter OK

--- tests/Browse/PosterCest.php
+++ tests/Browse/PosterCest.php
@@ -10,4 +10,11 @@ class PosterCest
         $I->amOnPage('/poster.php');
         $I->seeInCurrentUrl('/img/default.png');
     }
+
+
+    public function loadCoverWithWrongParameter(BrowseTester $I)
+    {
+        $I->amOnPage('/poster.php?posterId=324923949243932');
+        $I->seeInCurrentUrl('/img/default.png');
+    }
 }
