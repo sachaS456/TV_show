0d2551c00590bb900c5238ddb3b615022a3b976b
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-12 10:04:32 +0200

    load cover without parameter OK

--- tests/Browse/PosterCest.php
+++ tests/Browse/PosterCest.php
@@ -5,5 +5,9 @@ use Codeception\Example;
 use Tests\BrowseTester;
 class PosterCest
 {
-
+    public function loadCoverWithoutParameter(BrowseTester $I)
+    {
+        $I->amOnPage('/poster.php');
+        $I->seeInCurrentUrl('/img/default.png');
+    }
 }
