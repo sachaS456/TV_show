32f9e722663af65337e99d5f39b778aef909af25
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-12 09:10:50 +0200

    findById ajouté

--- tests/Crud/PosterCest.php
+++ tests/Crud/PosterCest.php
@@ -8,4 +8,10 @@ use Tests\CrudTester;
 
 class PosterCest
 {
+    public function findById(CrudTester $I)
+    {
+        $poster = Poster::findById(15);
+        $I->assertSame(15, $poster->getId());
+        $I->assertSame(file_get_contents(codecept_data_dir().'poster/poster15.jpeg'), $poster->getJpeg());
+    }
 }
