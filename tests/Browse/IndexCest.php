63fa379c5b9a90ff42dd4b880b462d007cb12e5a
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-12 09:38:46 +0200

    test checkAppWebPageHtmlStructure

--- tests/Browse/IndexCest.php
+++ tests/Browse/IndexCest.php
@@ -5,5 +5,15 @@ namespace Tests\Browse;
 use Tests\BrowseTester;
 class IndexCest
 {
-
+    public function checkAppWebPageHtmlStructure(BrowseTester $I)
+    {
+        $I->amOnPage('/');
+        $I->seeResponseCodeIs(200);
+        $I->seeInTitle('Séries TV');
+        $I->seeElement('.header');
+        $I->seeElement('.header h1');
+        $I->see('Séries TV', '.header h1');
+        $I->seeElement('.content');
+        $I->seeElement('.footer');
+    }
 }
