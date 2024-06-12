b29ed0f3eb94cebca5717043a0179d337d8102ec
Sacha Himber <sacha.himber@etudiant.univ-reims.fr>
2024-06-12 11:40:15 +0200

    TVshowCest.php: Ajout de la méthode checkAppWebPageHtmlStructure

--- tests/Browse/TVshowCest.php
+++ tests/Browse/TVshowCest.php
@@ -3,7 +3,18 @@ declare(strict_types=1);
 
 namespace Tests\Browse;
 
+use Codeception\Example;
+use Tests\BrowseTester;
+
 class TVshowCest
 {
-
+    public function checkAppWebPageHtmlStructure(BrowseTester $I)
+    {
+        $I->amOnPage('/season.php?TVshowId=3');
+        $I->seeResponseCodeIs(200);
+        $I->seeElement('.header');
+        $I->seeElement('.header h1');
+        $I->seeElement('.content');
+        $I->seeElement('.footer');
+    }
 }
