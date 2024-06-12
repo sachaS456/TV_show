ccfdea016a76a681c68a6ae97ed36c4ea934b640
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-12 09:13:45 +0200

    test Poster.PHP OK

--- tests/Crud/PosterCest.php
+++ tests/Crud/PosterCest.php
@@ -14,4 +14,11 @@ class PosterCest
         $I->assertSame(15, $poster->getId());
         $I->assertSame(file_get_contents(codecept_data_dir().'poster/poster15.jpeg'), $poster->getJpeg());
     }
+
+    public function findByIdThrowsExceptionIfCoverDoesNotExist(CrudTester $I)
+    {
+        $I->expectThrowable(EntityNotFoundException::class, function () {
+            Poster::findById(PHP_INT_MAX);
+        });
+    }
 }
